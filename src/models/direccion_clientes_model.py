11985464e7a2e9f7a3ffdc82442c0169c42e4503
Juan-Programer <juandavidunseyn@gmail.com>
2023-12-20 12:42:29 -0400

    uwu

--- src/models/direccion_clientes_model.py
+++ src/models/direccion_clientes_model.py
@@ -4,11 +4,11 @@ from src.config.model_base import Base, ModelBase
 
 class Direccion_clientesModel(Base, ModelBase):
     __tablename:str = "direccion_clientes"
-    calle: sa.Column = sa.Column(sa.String(45), nullable=False)
-    sector: sa.Column = sa.Column(sa.String(45), nullable=False)
-    comuna: sa.Column = sa.Column(sa.String(45), nullable=False)
-    numero: sa.Column = sa.Column(sa.String(45), nullable=False)
-    cuidad: sa.Column = sa.Column(sa.String(45), nullable=False)
+    calle: sa.Column = sa.Column(sa.String, nullable=False)
+    sector: sa.Column = sa.Column(sa.String, nullable=False)
+    comuna: sa.Column = sa.Column(sa.String, nullable=False)
+    numero: sa.Column = sa.Column(sa.String, nullable=False)
+    cuidad: sa.Column = sa.Column(sa.String, nullable=False)
     cliente_id: sa.Column = sa.Column(sa.Integer, sa.ForeignKey('clientes.id'), nullable=False)
     created_at: sa.Column = sa.Column( sa.DateTime, default = datetime.now(), nullable = False )
     updated_at: sa.Column = sa.Column( sa.DateTime, default = datetime.now(), nullable = False )
