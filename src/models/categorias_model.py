3e6bca56ef1b4d1e8fa0cbd4cd0b61e6e3cae1fb
Juan-Programer <juandavidunseyn@gmail.com>
2023-12-20 12:37:53 -0400

    camnio

--- src/models/categorias_model.py
+++ src/models/categorias_model.py
@@ -5,6 +5,6 @@ from src.config.model_base import Base, ModelBase
 class CategoriaModel(Base, ModelBase):
     __tablename__:str = "categorias"
     id: sa.Column = sa.Column( sa.Integer, primary_key = True)
-    nombre: sa.Column = sa.Column(sa.String(45), nullable=False)
+    nombre: sa.Column = sa.Column(sa.String, nullable=False)
     created_at: sa.Column = sa.Column( sa.DateTime, default = datetime.now(), nullable = False )
     updated_at: sa.Column = sa.Column( sa.DateTime, default = datetime.now(), nullable = False )
