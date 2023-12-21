e12b330c0fe6e0ff77ddff1b834d6371c6b465a4
Erik Mota Galindo <kenshimota@gmail.com>
2023-12-21 14:38:07 -0300

    checking relationship

--- console.py
+++ console.py
@@ -7,20 +7,23 @@ class ConsoleApp():
 	def run(self):
 		try:
 			nombre: str = input("Ingrese el nombre del producto: ")
-			precio_compra: float= input("Ingrese el precio de compra del producto: ")
-			precio_venta:  float= input("Ingrese el precio de venta del producto: ")
-			descripcion: str = input("Ingrese la descripcion del producto")
-			producto_inventario:str=int (input("Ingrese el numero de productos en el inventario"))
-			categoria_id: str = int(input("Ingrese la categoria.id "))
+			precio_compra: float= float(input("Ingrese el precio de compra del producto: "))
+			precio_venta:  float= float(input("Ingrese el precio de venta del producto: "))
+			descripcion: str = input("Ingrese la descripcion del producto: ")
+			producto_inventario: int = int (input("Ingrese el numero de productos en el inventario: "))
+			categoria_id: int = int(input("Ingrese la categoria.id: "))
 
 			
 			producto_service: ProductoService = ProductoService()
-			producto_service.create( nombre=nombre, 
-						     precio_compra=precio_compra,
-							 precio_venta=precio_venta, 
-							 descripcion=descripcion, 
-							 producto_inventario=producto_inventario,
-						     categoria_id=categoria_id)
+
+			producto_service.create(
+				nombre=nombre, 
+				precio_compra=precio_compra,
+				precio_venta=precio_venta, 
+				descripcion=descripcion, 
+				producto_inventario=producto_inventario,
+				categoria_id=categoria_id
+			)
 
 		except ValueError as e:
 			print(e)
--- src/services/producto_service.py
+++ src/services/producto_service.py
@@ -1,3 +1,4 @@
+from src.models.categorias_model import CategoriaModel
 from src.models.productos_model import ProductosModel
 from src.config.session_database import SessionDatabase
 from sqlalchemy import func
@@ -9,26 +10,41 @@ class ProductoService():
         session: SessionDatabase = SessionDatabase()
 
         try:
-            #Crear un producto y ver si ya es existente.
-            existing_producto = session.query(ProductosModel). filter(func.upper(ProductosModel.nombre) == func.upper(nombre)).first()
-            if existing_producto:
-                raise ValueError ("Ya existe este producto en esta base de datos")
+            # Crear un producto y ver si ya es existente.
+            existing_producto = session.query(ProductosModel)\
+                .filter(func.upper(ProductosModel.nombre) == func.upper(nombre))\
+                .first()
             
+
+            if existing_producto:
+                raise ValueError("Ya existe este producto en esta base de datos")
+
+
+            category = session.query(CategoriaModel)\
+                .filter(CategoriaModel.id == categoria_id)\
+                .first()
+            
+            if not category:
+                raise ValueError("la categoria seleccionada no existe")
+
             producto: ProductosModel = ProductosModel(
                 nombre=nombre,
                 precio_compra = precio_compra,
                 precio_venta=precio_venta,
                 descripcion = descripcion,
                 producto_inventario=producto_inventario,
-                categoria_id = categoria_id 
+                categoria_id = category.id
             )
 
             session.add(producto)
 
             created: ProductosModel = session.query(ProductosModel)\
-            .filter(ProductosModel.nombre == nombre)\
-            .first()
+                .filter(ProductosModel.nombre == nombre)\
+                .first()
+            
             session.commit()
+            
+            
             return created
         finally:
             session.close()
